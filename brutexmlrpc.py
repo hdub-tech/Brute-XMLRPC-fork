83a1129bf3c01822046a880bf876d20fe359a8dd
H Dub <14808878+hdub-tech@users.noreply.github.com>
2026-08-07 18:13:34 -0500

    feat(multicall): skip if WordPress 4.4 or later
    In 4.4, WP no longer tries to authenticate attempts once its has hit
    an invalid login attempt, making using the multicall feature of this
    tool useless unless the first user and first password were the match.
    https://github.com/WordPress/wordpress-develop/commit/75ab50c
    
    Fortunately, also in 4.4, they added the WP-API REST API infrastructure
    which is decectable preauth via /xmlrpc.php?rsd. So we can skip trying
    multicall if the host is post 4.4.
    https://wordpress.org/news/2015/12/clifford/

--- brutexmlrpc.py
+++ brutexmlrpc.py
@@ -252,6 +252,17 @@ async def brute_force_login(url, username, password, session):
 # ==================================================================================================
 # ==================================================================================================
 # ==================================================================================================
+async def detect_rest_api(url, session):
+    """Function detecting WP-API REST endpoint from /xmlrpc.php?rsd"""
+    headers = generate_random_headers(url)
+    async with session.get(url + '/xmlrpc.php?rsd', headers=headers, ssl=False) as resp:
+        response_text = await resp.text()
+        xmlroot = ET.fromstring(response_text.strip())
+        api_endpoints = xmlroot.findall('.//{*}api[@name="WP-API"]')
+        rest_api_detected = len(api_endpoints) == 1
+        if rest_api_detected:
+            logging.warning("WP-API REST endpoint detected - host is version 4.4 or later")
+        return rest_api_detected
 
 async def exploit_multicall(url, usernames, passwords, session):
     headers = generate_random_headers(url)
@@ -657,6 +668,14 @@ async def main():
         ).lower()
 
         if multicall_choice == "y":
+            rest_api_detected = await detect_rest_api(url, session)
+            if rest_api_detected:
+                logging.error(
+                    "system.multicall was modified in version 4.4 to invalidate all calls "
+                    "after an invalid one was detected. Try again without multicall."
+                )
+                return
+
             response_time = await start_multicall_async(
                 url + "/xmlrpc.php", users, passwords, session
             )
